0f8f960e7796716a90830da0c8ed3c172ec69994
Haya4Taka <takahiro@hayashiacBookea.lan>
2018-11-21 10:35:47 +0900

    Ruby_Version_Down

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
       user.email = auth.info.email || User.dummy_email(auth)
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
+      user.nickname = auth.info.nickname
        # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
